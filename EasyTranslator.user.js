616a67f6d84b3f6fbfd4d09cdc7079a97be5804c
Ultrasteve <609117264@qq.com>
2017-12-23 17:58:29 +0800

    2017-12-22

---
+++ EasyTranslator.user.js
@@ -0,0 +1,69 @@
+// ==UserScript==
+// @name         EasyTranslator
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  双击屏幕取词
+// @author       Nebulus
+// @include      *
+// @grant        GM_xmlhttpRequest
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    var trsBlock = createBlock();
+    document.body.appendChild(trsBlock);
+    window.onmousedown = () => hideElement(trsBlock);
+    window.onmouseup = (e) => translation(getWord(), trsBlock, e);
+})();
+
+function createBlock() {
+    var block = document.createElement("DIV");
+    block.style.cssText = "position: absolute; z-index: 999999; background-color: black; color: white;";
+    return block;
+}
+
+function hideElement(el) {
+    el.style.display = "none";
+    el.innerHTML = "";
+}
+
+function translation(word, el, e) {
+    if(word === null || word === undefined)
+        return null;
+    var result = "";
+    GM_xmlhttpRequest({
+        method: "GET",
+        url: "http://gre.kmf.com/vocab/ajaxquery?word="+word,
+        onload: function(response) {
+            result = JSON.parse(response.responseText).result.cn_trans;
+            console.log(result);
+            if(result !== undefined) {
+                el.style.left = ""+e.pageX+"px";
+                el.style.top = ""+e.pageY+"px";
+                el.innerHTML = result;
+                el.style.display = "block";
+            }
+        }
+    });
+    return result;
+}
+function getWord() {
+    var word = window.getSelection().toString();
+    console.log(word);
+    if(word === "")
+        return;
+    return word;
+}
+
+function mouseCoords(ev) {
+        if (ev.pageX || ev.pageY) {
+            return {x: ev.pageX, y: ev.pageY};
+        }
+        return {
+            x: ev.clientX + document.body.scrollLeft - document.body.clientLeft,
+            y: ev.clientY + document.body.scrollTop - document.body.clientTop
+        };
+}
+
